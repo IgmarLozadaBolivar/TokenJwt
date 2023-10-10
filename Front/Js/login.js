3626dc46d7cf8c46920e1641f5a7387dd5048852
DavidVargasDuarte <David.1025>
2023-10-10 10:32:20 -0500

    feat: :sparkles: The Front-End was implemented
    Son archivos nuevos y cada uno representa un solo commit lastimosamente

---
+++ Front/Js/login.js
@@ -0,0 +1,38 @@
+const urlAuth = "http://localhost:5183/User/validarUser";
+const headers = new Headers({ 'Content-Type': 'application/json' });
+const boton = document.getElementById('boton');
+
+boton.addEventListener("click", function (e) {
+    e.preventDefault();
+    autorizarUsuario();
+});
+
+async function autorizarUsuario() {
+    let inputUsuario = document.getElementById('username').value;
+    let inputPassword = document.getElementById('password').value;
+
+    let data = {
+        "username": inputUsuario,
+        "password": inputPassword
+    }
+
+    const config = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    };
+
+    try {
+        const response = await fetch(urlAuth, config);
+
+        if (response.ok) {
+            // La autenticación fue exitosa, redirige al usuario a la página Swagger
+            window.location.href = "http://localhost:5183/swagger";
+        } else {
+            // La autenticación falló, muestra un mensaje de error o redirige al usuario a la página de registro
+            alert("Autenticación fallida. Verifique sus credenciales o regístrese.");
+        }
+    } catch (error) {
+        console.error("Error al realizar la autenticación:", error);
+    }
+}
---
+++ Front/Js/register.js
@@ -0,0 +1,36 @@
+const urlRegister = "http://localhost:5183/User/registrar";
+const headers = new Headers({ 'Content-Type': 'application/json' });
+const boton = document.getElementById('botonRegistro');
+
+boton.addEventListener("click", function (e) {
+    e.preventDefault();
+    registrarUsuario();
+});
+
+async function registrarUsuario() {
+    let inputUsuario = document.getElementById('username').value;
+    let inputPassword = document.getElementById('password').value;
+
+    let data = {
+        "username": inputUsuario,
+        "password": inputPassword
+    }
+
+    const config = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+    };
+
+    try {
+        const response = await fetch(`${urlRegister}`, config);
+
+        if (response.status === 200) {
+            window.location.href = '../index.html';
+        } else {
+            console.error("La solicitud no fue exitosa. Estado:", response.status);
+        }
+    } catch (error) {
+        console.error("Error de red: ", error);
+    }
+}
