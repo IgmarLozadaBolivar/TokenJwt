41731f83b775403e748a1864d1785981928b0c97
DavidVargasDuarte <David.1025>
2023-10-10 10:30:09 -0500

    feat: :sparkles: Implementation of the extensions file

---
+++ Extension/ApplicationServiceExtension.cs
@@ -0,0 +1,13 @@
+namespace TokenJwt.Extension;
+
+public static class ApplicationServiceExtension
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+       services.AddCors(options =>
+       {
+           options.AddPolicy("CorsPolicy", builder =>
+           builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader());
+       });
+}
